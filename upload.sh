74ec54b93820be4fb225e023f679600018268a75
Sevati <17865169780@163.com>
2016-12-05 21:08:11 +0800

    add a post

--- upload.sh
+++ upload.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 hexo g
 hexo d
-git add .
-git commit -m"add a post"
-git push https://github.com/Sevati/sevati.github.io.git develop
 cd ./themes/yelee
 git add .
 git commit -m"modify theme"
 git push https://github.com/Sevati/sevati.github.io.git feature/theme
+cd ../../
+git add .
+git commit -m"add a post"
+git push https://github.com/Sevati/sevati.github.io.git develop
